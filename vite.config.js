69f52cedd24ff0b5f7abb41614c1212853e8f4ea
a791474 <a791474@gmail.com>
2024-01-31 05:27:43 +0800

    修正打包名稱

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production'
-  ? '/chd104/g6/back/'
+  ? '/chd104/g6/admin/'
   : '/',
   build: {
-    outDir: 'back'
+    outDir: 'admin'
   },
   plugins: [
     vue(),
