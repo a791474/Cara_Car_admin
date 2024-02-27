4a9f13c87f29853f045a6f7c8d203155ae19a167
a791474 <a791474@gmail.com>
2024-02-27 14:21:42 +0800

    修改sidebar名稱 修改getUsername的變數路徑

--- src/components/backTitle.vue
+++ src/components/backTitle.vue
@@ -5,12 +5,12 @@ import userStore from '@/stores/user'
 export default {
     data() {
         return {
-            userStoreData:userStore(), //呼叫user.js的Pinia
+            userStoreData: userStore(), //呼叫user.js的Pinia
         };
     },
     created() {
-        this.userData.admin_id = localStorage.getItem("userData") ? JSON.parse(localStorage.getItem("userData")).id:"";
-        this.userData.admin_name = localStorage.getItem("userData") ? JSON.parse(localStorage.getItem("userData")).name:"";
+        this.userData.admin_id = localStorage.getItem("userData") ? JSON.parse(localStorage.getItem("userData")).id : "";
+        this.userData.admin_name = localStorage.getItem("userData") ? JSON.parse(localStorage.getItem("userData")).name : "";
         // 判斷有沒有登入過
         this.checkLogin();
         // 把userData從Pinia確定有抽到此Vue檔中，並給admin_id定義
@@ -18,13 +18,13 @@ export default {
         //console.log(this.userData); //確定有抓到
 
         //獲取PHP資料之前先傳入admin_id，執行完PHP檔之後將資料傳入user.js內的userData陣列中，而Pinia定義的陣列由下方的mapState獲取
-        axios.get(`${import.meta.env.VITE_CARA_URL}/back/getUsername.php?admin_id=${admin_id}`) 
+        axios.get(`${import.meta.env.VITE_LPHP_URL}/back/getUsername.php?admin_id=${admin_id}`)
             .catch((error) => {
                 console.error("Error fetching data:", error);
             });
     },
     // mounted() {
-        
+
     // },
     computed: {
         //使用 mapState 輔助函數將/src/stores/user裡的state/data 映射在這裡
@@ -33,7 +33,7 @@ export default {
     },
     methods: {
         //使用 mapActions 輔助函數將/src/stores/user裡的actions/methods 映射在這裡
-        ...mapActions(userStore, ['checkLogin', 'updateToken','updateUserData']),
+        ...mapActions(userStore, ['checkLogin', 'updateToken', 'updateUserData']),
         logout() {
             // 調用pinia的updateToken
             this.$router.push('/')
