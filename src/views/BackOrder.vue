0589438e7c00a64e514b9e5e568e0571d809f72d
Kelly0518 <kelly05182@gmail.com>
2024-02-28 00:43:03 +0800

    fix backorder

--- src/views/BackOrder.vue
+++ src/views/BackOrder.vue
@@ -77,16 +77,25 @@ export default {
         },
     },
     methods: {
-        getOrderList(){
-            axios.get(`${import.meta.env.VITE_LPHP_URL}/back/backOrder.php`)
-        .then((response)=>{
-            this.orderList = response.data;
-            this.newOrderList = response.data;
-            this.formData = response.data;
-        })
-        .catch((error)=>{
-            console.error("Error fetching data:", error);
-        });
+        changeState() {
+            const newState = this.fromData.ord_del_state == true ? 1 : 0;
+            // const currentId =this.fromData.ord_id;
+            // console.log(newState);
+
+            const editItem = new FormData();
+            editItem.append("tableName", "ord_list")
+            editItem.append("ord_del_state", newState)
+            // editItem.append("ord_id", currentId)
+            // console.log(editItem);
+            try {
+                // 發送請求更新admin_state 到後端
+                axios.post(`${import.meta.env.VITE_LPHP_URL}/back/backOrder.php`, editItem, {
+                    headers: { "Content-Type": "multipart/form-data" },
+                });
+                console.log('Updated ord state:', newState);
+            } catch (error) {
+                console.error('Failed to update ord state:', error);
+            }
         },
         toggleStatus(index) {
             this.item[index].status = !this.item[index].status;
@@ -111,11 +120,76 @@ export default {
                 }
             })
         },
+        getOrderList(){
+        axios.get(`${import.meta.env.VITE_LPHP_URL}/back/backOrder.php`)
+        .then((response)=>{
+            const rawData = response.data;
+            const orderMap = new Map();
+        
+        // 合併相同訂單號碼的項目並加總金額
+        for (const item of rawData) {
+            if (!orderMap.has(item.ord_id)) {
+                orderMap.set(item.ord_id, {
+                    ord_id: item.ord_id,
+                    member_id: item.member_id,
+                    ord_date: item.ord_date,
+                    ord_reciever: item.ord_reciever,
+                    ord_city: item.ord_city,
+                    ord_district: item.ord_district,
+                    ord_address: item.ord_address,
+                    ord_phone: item.ord_phone,
+                    remark: item.remark,
+                    ord_ship: item.ord_ship,
+                    ord_sum: 0,
+                    ord_total: 0,
+                    ord_del_state: item.ord_del_state,
+                    products: []
+                });
+            }
+            const order = orderMap.get(item.ord_id);
+            order.ord_sum += item.ord_sum;
+            order.ord_total += item.ord_total;
+            order.products.push({
+                pro_id: item.pro_id,
+                pro_name: item.pro_name,
+                ord_qty: item.ord_qty,
+                pro_price: item.pro_price,
+                promo_ratio: item.promo_ratio,
+                pro_sale: item.pro_sale,
+                m_name: item.m_name
+            });
+        }
+        
+            this.orderList = Array.from(orderMap.values());
+            this.newOrderList = Array.from(orderMap.values());
+            this.formData = this.orderList[0];
+        })
+        .catch((error)=>{
+            console.error("Error fetching data:", error);
+        });
+    },
+        getProductsByOrderId(orderId) {
+            const products = [];
+            // 找到相同訂單編號的訂單
+            const order = this.newOrderList.find(item => item.ord_id === orderId);
+            // 如果找到了訂單
+            if (order) {
+                // 迭代訂單中的商品
+                for (const product of order.products) {
+                    products.push({
+                        productName: product.pro_name,
+                        quantity: product.ord_qty
+                    });
+                }
+            }
+            return products;
+        },
         showOrderDetails(item){
             this.formData = {
                 number: item.ord_id,
                 productName: item.pro_name,
                 quantity: item.ord_qty,
+                member_id: item.member_id,
                 memberName: item.m_name,
                 time: item.ord_date,
                 name: item.ord_reciever,
@@ -125,7 +199,21 @@ export default {
                 freight: item.ord_ship,
                 total: item.ord_total,
                 remark: item.remark,
+                products: [],
             };
+            console.log(item);
+            console.log(this.formData);
+            this.formData.products = [];
+            
+            for (const product of item.products) {
+            // 創建新的商品對象，包含商品名稱和數量
+            const newProduct = {
+            productName: product.pro_name,
+            quantity: product.ord_qty
+            };
+            // 將新的商品對象添加到 formData 的 products 屬性中
+            this.formData.products.push(newProduct);
+            }
             this.value = true;
         },
     },
@@ -158,6 +246,7 @@ export default {
                             <li>出貨狀態</li>
                         </ul>
                     </div>
+                    
                     <div class="orderListContent" v-for="(item,index) in paginated" :key="index">
                         <div class="searchButton">
                             <button @click="showOrderDetails(item)" type="primary" class="searchBtn">查詢</button>
@@ -170,7 +259,7 @@ export default {
                         <div class="switch">
                             <Space direction="vertical">
                                 <Space>
-                                    <Switch size="large" v-model="item.ord_del_state" :true-value="1" :false-value="0" @on-change="changeState(item)" class="switchButton" >
+                                    <Switch size="large" v-model="item.ord_del_state" :true-value="1" :false-value="0" @on-change="changeState" class="switchButton" >
                                         <template #open>
                                             <!-- v-if="item.ord_del_state" -->
                                             <span >已出</span>
@@ -184,7 +273,8 @@ export default {
                         </div>
                     </div>
                 </div>
-                
+                <!-- {{newOrderList[2]}} -->
+                <!-- {{formData}} -->
                 <PageNumber :totalPages="totalPages" :currentPage="currentPage" @pageChange="changePage"/>
             </div>
         </div>
@@ -211,20 +301,25 @@ export default {
         </Row>
         <!-- <Row :gutter="32" v-for="item in formData" :key="item"> -->
             <!-- 商品&數量會有多個，v-for怎麼寫 -->
-        <Row :gutter="32"> 
-            <Col span="12">
-                <FormItem label="商品名稱" label-position="top">
-                    <Input v-model="formData.productName" placeholder="please enter product name" />
-                </FormItem>
-            </Col>
-            <Col span="12">
-                <FormItem label="數量" label-position="top">
-                    <Input v-model="formData.quantity" placeholder="please enter quantity" />
-                </FormItem>
-            </Col>
-        </Row>
+            <!-- {{formData}} -->
+            <!-- {{orderList}} -->
+            <!-- {{newOrderList}} -->
+        <!-- <div v-for="(order, orderIndex) in newOrderList" :key="orderIndex"> -->
+            <Row :gutter="32" v-for="(product, index) in formData.products" :key="index">
+                <Col span="12" >
+                    <FormItem label="商品名稱" label-position="top">
+                        <Input v-model="product.productName" placeholder="please enter product name" />
+                    </FormItem>
+                </Col>
+                <Col span="12" >
+                    <FormItem label="數量" label-position="top">
+                        <Input v-model="product.quantity" placeholder="please enter quantity" />
+                    </FormItem>
+                </Col>
+            </Row>
+        <!-- </div> -->
         <Row :gutter="32">
-            <Col span="12">
+            <Col span="12" >
                 <FormItem label="購買人" label-position="top">
                     <Input v-model="formData.memberName" placeholder="please purchaser name" />
                 </FormItem>
@@ -255,7 +350,7 @@ export default {
         <Row :gutter="32">
             <Col span="12">
                 <FormItem label="付款方式" label-position="top">
-                    <Input v-bind="信用卡付款"  />
+                    <Input placeholder="信用卡付款"  />
                 </FormItem>
             </Col>
             <Col span="12">
